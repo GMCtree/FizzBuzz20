13ae3ae41a36c1c2b77830c38ac86c1b522b4bdc
GMCtree <gciavaglia95@gmail.com>
2017-03-25 17:55:53 -0400

    added ruby

---
+++ ruby.rb
@@ -0,0 +1,16 @@
+for i in 1..100
+	if i % 3 == 0 and i % 5 == 0 then
+		puts "FizzBuzz"
+	elsif i % 5 == 0 and i % 7 == 0 then
+		puts "BizzBuzz"
+	elsif i % 3 == 0 then
+		puts "Fizz"
+	elsif i % 5 == 0 then
+		puts "Buzz"
+	elsif i % 7 == 0 then
+		puts "Bizz"
+	else
+		puts i
+	end
+end
+
